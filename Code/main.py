c7e7fa94fc0a460cf939573ae7528bd1543aae80
Romain Castellarnau <77051622+RomainCastellarnau@users.noreply.github.com>
2024-01-21 03:40:03 +0100

    Update

--- Code/main.py
+++ Code/main.py
@@ -1,26 +1,10 @@
 from pca import PCA  # Import the PCA class
-import os
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-sns.set_style("darkgrid")
-
 
 if __name__ == "__main__":
-    path = os.path.join(os.path.dirname(os.getcwd()), "Data")
-    returns = pd.read_excel(
-        os.path.join(path, "Data.xlsx"), sheet_name="RETURNS"
-    ).rename(columns={"Unnamed: 0": "Date"})
-    returns = returns.set_index("Date")
-    returns.head()
-    stocks = returns.columns.tolist()
-
     # Def the PCA model
-    model = PCA(returns, stocks)
+    model = PCA()
 
-    # Retrive the attribute
+    # Retrieve the attribute
     my_data = model.returns
     cov = model.cov_matrix
     eigen_values = model.eigenvalues
