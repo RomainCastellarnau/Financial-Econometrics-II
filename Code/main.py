c882b02572f099e16d83af21aa5dbdc84b16860f
Romain Castellarnau <77051622+RomainCastellarnau@users.noreply.github.com>
2024-01-21 20:45:56 +0100

    Update

--- Code/main.py
+++ Code/main.py
@@ -4,15 +4,16 @@ if __name__ == "__main__":
     
     ############################################
 
-    # Initialization of the class
+    # Initialization of the class:
     CPtf = CorePtf()
 
-    # Question 1 - We first compute the PCA model of the returns of the 47 stocks returns
-    # - The covariance matrix is computed using the stocks returns (not standardized) and stored in the class
-    # - The PCA full model is computed by calling the function compute_full_model on the standardized stocks returns
-    # - First PC loading sign is checked and pc scores 1 sign are flipped if needed
-    # - The reduced form model is then built by selecting the first 3 PC scores (Bai and Ng 2002 criteria)
-    # - PC scores are being rescaled to the volatility of the benchmark (SX5E)
+    # Question 1
+    # - We first compute the PCA model of the returns of the 47 stocks returns.
+    # - The covariance matrix is computed using the stocks returns (not standardized) and stored in the class.
+    # - The PCA full model is computed by calling the function compute_full_model on the standardized stocks returns.
+    # - First PC loading sign is checked and pc scores 1 sign are flipped if needed.
+    # - The reduced form model is then built by selecting the first 3 PC scores (Bai and Ng 2002 criteria).
+    # - PC scores are being rescaled to the volatility of the benchmark (SX5E).
 
     CPtf.compute_covariance_matrix()  # Compute the covariance matrix of the returns not standardized
     CPtf.compute_full_model()  # Compute the PCA model with 47 Principal Components
@@ -20,24 +21,26 @@ if __name__ == "__main__":
     CPtf.check_loading_sign()  # Check the sign of the loadings of the first PC
     CPtf.rescale_pc()  # Rescale the PC scores to the same volatility as that of the benchmark
 
-    # We plot the cumulative variance explained by the PC
+    # We plot the cumulative variance explained by the PC:
     CPtf.plot_cumulative_variance_explained()
 
-    # We then plot the variance explained by the selected PC with criterion
+    # We then plot the variance explained by the selected PC with criterion:
     CPtf.plot_variance_explained()
 
     ############################################
 
-    # Question 2 - Each stocks returns are regressed on all the selected PC scores using an OLS regression
-    # - The regression coefficients are then stocked and will be used to build the factor replicating portfolios;
+    # Question 2
+    # - Each stocks returns are regressed on all the selected PC scores using an OLS regression.
+    # - The regression coefficients are then stocked and will be used to build the factor replicating portfolios.
     # These coefficients represent the exposure of each stock returns to the K core equity factors.
 
     CPtf.pca_model()
 
     ############################################
 
-    # Question 3 - The first core equity replicating portfolios is then built using the regression coefficients and
-    # an optimization routine (minimize the variance of the portfolio)
+    # Question 3
+    # - The first core equity replicating portfolios is then built using the regression coefficients and
+    # an optimization routine (minimize the variance of the portfolio).
 
     CPtf.compute_core_equity_ptf()
     core_eq_composition = CPtf.core_equity_ptf
@@ -51,9 +54,10 @@ if __name__ == "__main__":
 
     ############################################
 
-    # Question 4 - We can now compute the returns of the first core equity factor and estimate its alpha against the benchmark (SX5E)
-    # - The alpha is computed using a simple OLS regression of the replicating portfolio returns against the benchmark returns
-    # - The relevant performance metrics are also computed and stored in a dictionary (see CorePtf.py)
+    # Question 4
+    # - We can now compute the returns of the first core equity factor and estimate its alpha against the benchmark (SX5E).
+    # - The alpha is computed using a simple OLS regression of the replicating portfolio returns against the benchmark returns.
+    # - The relevant performance metrics are also computed and stored in a dictionary (see CorePtf.py).
 
     CPtf.alpha_core_ptf()
     core_eq_1_ptf_stat = CPtf.ptf_stats
@@ -62,20 +66,21 @@ if __name__ == "__main__":
     for key, value in core_eq_1_ptf_stat.items():
         print(key, ":", value, "\n")
 
-    # We can now plot the cumulative returns of the first core equity factor against the benchmark (SX5E)
+    # We can now plot the cumulative returns of the first core equity factor against the benchmark (SX5E):
     CPtf.plot_compared_performance()
 
-    # And analyze its composition
+    # And analyze its composition:
     CPtf.plot_core_ptf_comp()
 
     ############################################
 
-    # Question 5 - We are now interested in the impact of the estimation error of the variance covariance matrix on the performance of the core equity factor
-    # - We randomly choose a sample containing 95% of the returns / compute the associated variance covariance matrix
-    # - We then build for each different variance-covariance matrix the core equity factor replicating portfolio and compute its alpha
-    # - Having obtained the distribution of the alpha, we can compute it's mean and standard deviation of the alpha and build a confidence interval
-    # This process is done by the function simulate_alpha_impact() (this takes a minute and a half to run)
-    # alpha_stats is a dictionary containing the mean, the standard deviation of the estimated alphas and a 95% confidence interval
+    # Question 5
+    # - We are now interested in the impact of the estimation error of the variance covariance matrix on the performance of the core equity factor.
+    # - We randomly choose a sample containing 95% of the returns / compute the associated variance covariance matrix.
+    # - We then build for each different variance-covariance matrix the core equity factor replicating portfolio and compute the associated alpha.
+    # - Having obtained the distribution of the simulated alphas, we can compute the mean and standard deviation of the alphas and build a confidence interval.
+    # This process is done by the function simulate_alpha_impact() (this takes a minute and a half to run).
+    # alpha_stats is a dictionary containing the mean, the standard deviation of the estimated alphas and a 95% confidence interval.
 
     alpha_stats = CPtf.simulate_alpha_impact()
 
@@ -83,10 +88,10 @@ if __name__ == "__main__":
     for key, value in alpha_stats.items():
         print(key, ":", value, "\n")
 
-    # We can now plot the distribution of the alpha of the first core equity factor replicating portfolio
+    # We can now plot the distribution of the alpha of the first core equity factor replicating portfolio:
     CPtf.plot_alpha_distribution()
 
-    # And the Mean-Volatility space of the first core equity factor replicating portfolio simulated
+    # And the Mean-Volatility space of the first core equity factor replicating portfolio simulated:
     CPtf.plot_mean_vol_sim()
 
     ############################################
